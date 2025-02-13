23f0c073be02bd0c57dea1d56e37e7f482c0c4e9
Caleb Orchard <caleborchard08@gmail.com>
2025-02-13 09:43:25 -0500

    Change variables from 'long' type to 'BigInteger' so that there is no integer overflow problem. Change while loop check to arbitrary value instead of integer overflow check.

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,22 @@
+import java.math.BigInteger;
+
 public class Main
 {
     public static void main(String[] args)
     {
         System.out.println("Fibonacci Sequence");
 
-        long fib1 = 0;
-        long fib2 = 0;
-        long current = 1;
-        while(current > 0)
+        int counter = 0;
+        BigInteger fib1 = new BigInteger("0");
+        BigInteger fib2 = new BigInteger("0");
+        BigInteger current = new BigInteger("1");
+        while(counter < 1000)
         {
+            counter++;
             System.out.println(current);
             fib1 = fib2;
             fib2 = current;
-            current = fib1 + fib2;
+            current = fib1.add(fib2);
         }
     }
 }
